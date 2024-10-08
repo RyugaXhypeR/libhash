c25c703a5fded4742adfd3c98b147569dc9fc7bb
RyugaXhypeR <ryugashyper@gmail.com>
2024-10-08 12:05:03 +0000

    feat: `ROT` functions depend on size of `x`

--- include/sha.h
+++ include/sha.h
@@ -1,11 +1,11 @@
 #ifndef _SHA
 #define _SHA
 
-#define ROTL(x, n, w) ((x) << (n) | (x) >> (w) - (n))
-#define ROTR(x, n, w) ((x) >> (n) | (x) << (w) - (n))
+#define CH(x, y, z) (((x) & (y)) | (~(x) & (z)))
+#define PARITY(x, y, z) ((x) ^ (y) ^ (z))
+#define MAJ(x, y, z) (((x) & (y)) ^ ((x) & (z)) ^ ((y) & (z)))
 
-#define Ch(x, y, z) (((x) & (y)) ^ (~(x) & (z)))
-#define Parity(x, y, z) ((x) ^ (y) ^ (z))
-#define Maj(x, y, z) (((x) & (y)) ^ ((x) & (z)) ^ ((y) & (z)))
+#define ROTL(x, n) (((x) << (n)) | ((x) >> ((sizeof(x) * 8) - (n))))
+#define ROTR(x, n) (((x) >> (n)) | ((x) << ((sizeof(x) * 8) - (n))))
 
 #endif /* _SHA */
