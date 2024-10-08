89fcee4d2228b94ea0a58de023194178d0edef40
RyugaXhypeR <ryugashyper@gmail.com>
2024-10-08 14:52:28 +0000

    feat: added test cases for `sha224`

--- tests/sha.c
+++ tests/sha.c
@@ -22,7 +22,7 @@ check32_bit_hash(uint32_t *hash, size_t hash_size, char *hexdigest, char *digest
 void
 test_sha1(void) {
     uint32_t hash[5];
-    char digest[160];
+    char digest[64];
 
     sha1("", hash);
     check32_bit_hash(hash, 5, "da39a3ee5e6b4b0d3255bfef95601890afd80709", digest);
@@ -34,7 +34,23 @@ test_sha1(void) {
     check32_bit_hash(hash, 5, "84983e441c3bd26ebaae4aa1f95129e5e54670f1", digest);
 }
 
+void
+test_sha224(void) {
+    uint32_t hash[7];
+    char digest[64];
+
+    sha224("", hash);
+    check32_bit_hash(hash, 7, "d14a028c2a3a2bc9476102bb288234c415a2b01f828ea62ac5b3e42f", digest);
+
+    sha224("abc", hash);
+    check32_bit_hash(hash, 7, "23097d223405d8228642a477bda255b32aadbce4bda0b3f7e36c9da7", digest);
+
+    sha224("abcdbcdecdefdefgefghfghighijhijkijkljklmklmnlmnomnopnopq", hash);
+    check32_bit_hash(hash, 7, "75388b16512776cc5dba5da1fd890150b0c6455cb4f58b1952522525", digest);
+}
+
 int
 main(void) {
     test_sha1();
+    test_sha224();
 }
