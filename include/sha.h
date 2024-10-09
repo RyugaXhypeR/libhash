a628c5cf071e61f4076bc447a312c2b346529b9c
RyugaXhypeR <ryugashyper@gmail.com>
2024-10-09 17:32:08 +0000

    feat: added round fns for 512-bit hash

--- include/sha.h
+++ include/sha.h
@@ -16,6 +16,13 @@
 #define SIGMA256_0_SMALL(x) (ROTR(x, 7) ^ ROTR(x, 18) ^ ((x) >> 3))
 #define SIGMA256_1_SMALL(x) (ROTR(x, 17) ^ ROTR(x, 19) ^ ((x) >> 10))
 
+#define SIGMA512_0_BIG(x) (ROTR(x, 28) ^ ROTR(x, 34) ^ ROTR(x, 39))
+#define SIGMA512_1_BIG(x) (ROTR(x, 14) ^ ROTR(x, 18) ^ ROTR(x, 41))
+
+#define SIGMA512_0_SMALL(x) (ROTR(x, 1) ^ ROTR(x, 8) ^ ((x) >> 7))
+#define SIGMA512_1_SMALL(x) (ROTR(x, 19) ^ ROTR(x, 61) ^ ((x) >> 6))
+
+
 void sha1(char *message, uint32_t *hash);
 void sha224(char *message, uint32_t *hash);
 void sha256(char *message, uint32_t *hash);
