460db56bbaab1ebefeb9675a4ee8c6a16e4412bd
RyugaXhypeR <ryugashyper@gmail.com>
2024-10-09 17:37:52 +0000

    feat: added implementation for `sha384`

--- src/sha.c
+++ src/sha.c
@@ -329,3 +329,51 @@ sha512_schedule(uint8_t *message, int t) {
     return w[t];
 }
 
+void
+sha384(char *message, uint64_t *hash) {
+    uint8_t *padded_msg = (uint8_t *)message;
+    uint128_t message_len = strlen(message);
+    uint128_t msg_bit_len = message_len * 8;
+    uint64_t num_blocks = block128_len(msg_bit_len) / 1024;
+
+    pad128_resize(&padded_msg, msg_bit_len);
+    pad128(&padded_msg, msg_bit_len);
+
+    hash[0] = 0xcbbb9d5dc1059ed8;
+    hash[1] = 0x629a292a367cd507;
+    hash[2] = 0x9159015a3070dd17;
+    hash[3] = 0x152fecd8f70e5939;
+    hash[4] = 0x67332667ffc00b31;
+    hash[5] = 0x8eb44a8768581511;
+    hash[6] = 0xdb0c2e0d64f98fa7;
+    hash[7] = 0x47b5481dbefa4fa4;
+
+    for (uint64_t i = 0; i < num_blocks; i++) {
+        uint64_t a = hash[0], b = hash[1], c = hash[2], d = hash[3], e = hash[4], f = hash[5], g = hash[6], h = hash[7];
+        uint8_t *block = padded_msg + i * 128;
+
+        for (int t = 0; t < 80; t++) {
+            uint64_t temp1 = h + SIGMA512_1_BIG(e) + CH(e, f, g) + K64_80[t] + sha512_schedule(block, t);
+            uint64_t temp2 = SIGMA512_0_BIG(a) + MAJ(a, b, c);
+            h = g;
+            g = f;
+            f = e;
+            e = d + temp1;
+            d = c;
+            c = b;
+            b = a;
+            a = temp1 + temp2;
+        }
+
+        hash[0] += a;
+        hash[1] += b;
+        hash[2] += c;
+        hash[3] += d;
+        hash[4] += e;
+        hash[5] += f;
+        hash[6] += g;
+        hash[7] += h;
+    }
+
+    free(padded_msg);
+}
