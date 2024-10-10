cb9b4d3d48134ca7a161d392bba462a4b2af89cb
RyugaXhypeR <ryugashyper@gmail.com>
2024-10-10 09:11:27 +0000

    feat: added test cases for `sha512`

--- tests/sha.c
+++ tests/sha.c
@@ -135,10 +135,49 @@ test_sha384(void) {
                      digest);
 }
 
+void
+test_sha512(void) {
+    uint64_t hash[8];
+    char digest[384 + 1];
+
+    /* Single block */
+    sha512("", hash);
+    check64_bit_hash(hash, 8,
+                     "cf83e1357eefb8bdf1542850d66d8007d620e4050b5715dc83f4a921d36ce9ce"
+                     "47d0d13c5d85f2b0ff8318d2877eec2f63b931bd47417a81a538327af927da3e",
+                     digest);
+
+    sha512("abc", hash);
+    check64_bit_hash(hash, 8,
+                     "ddaf35a193617abacc417349ae20413112e6fa4e89a97ea20a9eeee64b55d39a"
+                     "2192992a274fc1a836ba3c23a3feebbd454d4423643ce80e2a9ac94fa54ca49f",
+                     digest);
+
+    /* Double block */
+    sha512(
+        "abcdefghbcdefghicdefghijdefghijkefghijklfghijklmghijklmnhijklmnoijklmnopjklmnopqklmnopqrlmnopqrsmnopqrstnopqrs"
+        "tu",
+        hash);
+    check64_bit_hash(hash, 8,
+                     "8e959b75dae313da8cf4f72814fc143f8f7779c6eb9f7fa17299aeadb6889018"
+                     "501d289e4900f7e4331b99dec4b5433ac7d329eeb6dd26545e96e55b874be909",
+                     digest);
+
+    /* Multi-block */
+    char message[1000000];
+    memset(message, 'a', sizeof message);
+    sha512(message, hash);
+    check64_bit_hash(hash, 8,
+                     "e718483d0ce769644e2e42c7bc15b4638e1f98b13b2044285632a803afa973eb"
+                     "de0ff244877ea60a4cb0432ce577c31beb009c5c2c49aa2e4eadb217ad8cc09b",
+                     digest);
+}
+
 int
 main(void) {
     test_sha1();
     test_sha224();
     test_sha256();
     test_sha384();
+    test_sha512();
 }
